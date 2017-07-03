6e45efbd286b6be708d807107ee6e90fac47fcac
DinoPeople <dp@DinoPeopleui-MacBook-Pro.local>
2017-07-03 22:04:42 +0900

    Initial Commit

---
+++ DinoPeopleFrame/DinoPeopleFrame.h
@@ -0,0 +1,19 @@
+//
+//  DinoPeopleFrame.h
+//  DinoPeopleFrame
+//
+//  Created by DinoPeople on 2017. 7. 3..
+//  Copyright © 2017년 DinoPeople. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DinoPeopleFrame.
+FOUNDATION_EXPORT double DinoPeopleFrameVersionNumber;
+
+//! Project version string for DinoPeopleFrame.
+FOUNDATION_EXPORT const unsigned char DinoPeopleFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DinoPeopleFrame/PublicHeader.h>
+
+
